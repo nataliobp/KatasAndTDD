8f73049ae24439bd12bc458cfdc07255e546775a
nataliobp <natalio.benitez@gmail.com>
2017-11-30 14:55:12 +0100

    bubble sorting, dfs and graphs, queues with stacks

---
+++ src/test/java/com/natalio/connectedcellingrid/ConnectedCellInGridTest.java
@@ -0,0 +1,20 @@
+package test.java.com.natalio.connectedcellingrid;
+
+import main.java.com.natalio.connectedcellingrid.ConnectedCellInGrid;
+import main.java.com.natalio.connectedcellingrid.ConnectedCellInGrid.Grid;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class ConnectedCellInGridTest {
+    @Test
+    public void gridOf4x4Returns5() throws Exception {
+        int[][] input = new int[][]{{1,1,0,0}, {0,1,1,0}, {0,0,1,0}, {1,0,0,0}};
+        Grid grid = new Grid(4, 4, input);
+
+        assertEquals(5, grid.numCellsInLargestRegion());
+    }
+}
---
+++ src/test/java/com/natalio/twostacksqueue/TwoStacksQueueTest.java
@@ -0,0 +1,29 @@
+package test.java.com.natalio.twostacksqueue;
+
+import main.java.com.natalio.twostacksqueue.TwoStacksQueue.MyQueue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class TwoStacksQueueTest {
+    @Test
+    public void lastElementShouldBe14() throws Exception {
+        MyQueue<Integer> q = new MyQueue<>();
+        q.enqueue(42);
+        q.dequeue();
+        q.enqueue(14);
+        int peek1 = q.peek();
+        q.enqueue(28);
+        int peek2 = q.peek();
+        q.enqueue(60);
+        q.enqueue(78);
+        q.dequeue();
+        q.dequeue();
+
+        assertEquals(14, peek1);
+        assertEquals(14, peek2);
+    }
+}
